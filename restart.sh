377bf628f20116b09dd2b883e780f60a94edbad9
Jimb Esser <wasteland@gmail.com>
2015-03-09 18:14:27 -0700

    Added ATITD.

---
+++ restart.sh
@@ -0,0 +1,2 @@
+sudo su noderunner -c "screen -S automato_server -X quit"
+sudo /etc/rg3d/automatoserver.sh
--- update.js
+++ update.js
@@ -30,7 +30,11 @@ function getManifest(base_dir, next, base_mode) {
             doit(fn, done);
           } else if (stat.isFile()) {
             var ext = path.extname(fn).toLowerCase();
-            if (ext === '.png' || ext === '.wav' || path.basename(path.dirname(fn)) === 'scripts' || base_mode && path.basename(fn) !== 'manifest.txt') {
+            if (ext === '.png' || ext === '.wav' || path.basename(path.dirname(fn)) === 'scripts' ||
+              base_mode && path.basename(fn) !== 'manifest.txt' ||
+              path.basename(path.dirname(fn)) === 'data' && path.basename(fn) === 'charTemplate.txt' ||
+              path.basename(path.dirname(fn)) === 'ATITD' && ext === '.txt' // carrot_config.txt, ThistleReference.txt
+            ) {
               ++left;
               fs.readFile(fn, function(err, data) {
                 var crc = crc32.crc32(data);
